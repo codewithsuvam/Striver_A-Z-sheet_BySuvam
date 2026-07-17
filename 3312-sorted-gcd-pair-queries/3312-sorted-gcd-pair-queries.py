3e16b639c0c1edab747ca44a5df86622049413da
Suvam Shaw <suvshaw22@gmail.com>
2026-07-17 19:08:49 +0530

    Time: 703 ms (30%), Space: 41.2 MB (25%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.py
@@ -0,0 +1,23 @@
+from typing import List
+from collections import Counter
+from itertools import accumulate
+from bisect import bisect_right
+
+class Solution:
+    def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
+        mx = max(nums)
+        freq = Counter(nums)
+
+        # cnt_g[g] = number of pairs whose gcd is exactly g
+        cnt_g = [0] * (mx + 1)
+
+        for g in range(mx, 0, -1):
+            total = 0
+            for x in range(g, mx + 1, g):
+                total += freq[x]
+                cnt_g[g] -= cnt_g[x]
+            cnt_g[g] += total * (total - 1) // 2
+
+        prefix = list(accumulate(cnt_g))
+
+        return [bisect_right(prefix, q) for q in queries]
