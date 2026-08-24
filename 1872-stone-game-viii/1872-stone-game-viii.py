da3611f51fc9931bb9eda4749f738987151dacd7
Suvam Shaw <suvshaw22@gmail.com>
2026-08-24 21:36:43 +0530

    Time: 673 ms (67.46%), Space: 32.9 MB (82.84%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        n = len(stones)
+
+        # Prefix sums
+        prefix = [0] * n
+        prefix[0] = stones[0]
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + stones[i]
+
+        # Start with the final possible score
+        ans = prefix[n - 1]
+
+        # Work backwards
+        for i in range(n - 2, 0, -1):
+            ans = max(ans, prefix[i] - ans)
+
+        return ans
